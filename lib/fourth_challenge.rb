63737203cd1a5045429a978a2bc2948f86ae8cee
Nicholas  Luong <nckluong@gmail.com>
2018-08-25 21:45:44 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {:name => "Benvolio", :age => "17", :attitude => "worried"}, 
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,8 +16,6 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-        {:name => "Benvolio", :age => "17", :attitude => "worried"}, 
-        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"},
         {:name => "Steven", :age=> "30", :attitude => "confused"},
         {:name => "Nurse", :age => "44", :attidue => "worried"}
       ]
